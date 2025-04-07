f5effc3be0b404084e1b09e2328837d2f49aef38
Jean-Luc Robitaille <jlr@robitaille.us>
2025-04-07 13:47:37 -0400

    Worked on encrypt

--- encrypt.py
+++ encrypt.py
@@ -1,42 +1,59 @@
 from hashlib import sha256
-from utils import meshmaker, xorutil
+from utils import meshmaker, xorutil, esbox
 import random
 
 def cryptoround(plaintext, password, rounds):
-    rounds=0
-    cryptpass=sha256(password.encode('utf-8')).hexdigest()
-    random.seed(int(cryptpass,16))
-    passmesh = meshmaker(cryptpass)
+    passmesh = meshmaker(password)
     plainmesh = meshmaker(plaintext)
+    print(plaintext)
+    print(password)
+    print(plainmesh)
+    print(passmesh)
     for i in range(8):
         for j in range(8):
+            plainmesh[i][j]=int(plainmesh[i][j],16)
+            print(str(i)+"   "+str(j))
+            print("part1")
             for k in range(8):
-                plainmesh[i][j]=xorutil(plainmesh[i][j],passmesh[i][k])
+                plainmesh[i][j] = xorutil(plainmesh[i][j], int(passmesh[i][k],16))
+            print("part2")
             for k in range(8):
-                plainmesh[i][j]=xorutil(plainmesh[i][j],passmesh[k][j])
-    print(plaintext)
-    crypttext=""
-    for i in plainmesh:
-        for j in i:
-            crypttext+=str(j)
-    print(crypttext)
-    if(rounds>0):
-        return round(crypttext,cryptpass,rounds-1)
+                
+                plainmesh[i][j] = xorutil(plainmesh[i][j], int(passmesh[k][j],16))
+    # Convert mesh back to string
+    out = ''.join([''.join(str(row)) for row in plainmesh])
+    out = esbox(out)
+    if rounds > 0:
+        new_password = esbox(out)
+        return encrypt(out, new_password, rounds - 1)
     else:
-        return crypttext
+        return out
 
 def encrypt(plaintext, password, rounds):
-    out=""
-    while(len(plaintext)>64):
-        out+=cryptoround(plaintext[:64],password,rounds)
-        plaintext=plaintext[64:]
-    while(len(plaintext)<64):
-        plaintext=plaintext+random.randbytes(1).hex()
-    while(len(plaintext)>64):
-        plaintext=plaintext[:64]
-    out+=cryptoround(plaintext, password, rounds)
-    return "hello7cd5139d4a0a8e04a8ed6261d94124f34841e3ce0c8cf4c29467533acdc"
-
+    cryptpass = sha256(password.encode('utf-8')).hexdigest()
+    print(cryptpass)
+    plaintext=str(plaintext.encode().hex())
+    print(plaintext)
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     
+    random.seed(int(cryptpass, 16))
+    out = ""
+    # Split into 64-byte blocks
+    while len(plaintext) >= 64:
+        block = plaintext[:64]
+        encrypted_block = cryptoround(block, cryptpass, rounds)
+        out += encrypted_block
+        plaintext = plaintext[64:]
+    # Pad remaining block if needed
+    if len(plaintext) > 0:
+        padding_length = 64 - len(str(plaintext))
+        padding = random.randbytes(padding_length).hex()
+        plaintext += padding
+        plaintext=plaintext[:64]
+        encrypted_block = cryptoround(plaintext, cryptpass, rounds)
+        out += encrypted_block
+    return out
 
-encrypt("hello","Printme",5)
+
+
+encrypt("hello", "Printme", 5)
